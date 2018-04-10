8d766d720ebbdca3622d14016b1ee622fe481a4c
amalkriz <amalkhd@gmail.com>
2018-04-11 00:40:44 +0530

    products page

---
+++ products.php
@@ -0,0 +1,205 @@
+<?php include('includes/header.php') ?>
+<section class="products">
+    <section class="products-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-xs-12 left-content">
+                    <h1>REFRIGERATION
+                        <br>
+                        <span>SERVICE PARTS</span>
+                    </h1>
+                    <p>Bringing You the Industry's Most Competitive Pricing for All of Your HVAC needs...</p>
+                </div>
+                <div class="col-sm-6 col-xs-12 right-content">
+                    <button class="btn btn-white">Quick Contact</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="product-list hidden-xs">
+        <section class="item">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 no-padding top">
+                        <div class="col-sm-4 col-md-3 content-div">
+                            <div class="products-content text-right">
+                                <h2>Refrigeration
+                                    <br> Compressor
+                                </h2>
+                                <p class="mt-30 mb-30">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                </p>
+                                <button class="btn btn-blue">view products</button>
+                            </div>
+                        </div>
+
+
+                        <img src="assets/img/products/pro-1.png" alt="" class="img-responsive w-100">
+                    </div>
+                    <div class="bottom">
+                        <div class="col-xs-12 no-padding">
+                            <div class="col-sm-4 col-md-3 content-div">
+                                <div class="products-content">
+                                    <h2>Refrigeration
+                                        <br> Replacement Condensing Unit &
+                                        <br>Evaporators
+                                    </h2>
+                                    <p class="mt-30 mb-30">
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                    </p>
+                                    <button class="btn btn-blue">view products</button>
+                                </div>
+                            </div>
+                            <img src="assets/img/products/pro-2.png" alt="" class="img-responsive w-100">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="item-2">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 no-padding top">
+                        <div class="col-sm-4 col-md-3 content-div">
+                            <div class="products-content">
+                                <h2>Refrigeration
+                                    <br> compressor parts
+                                </h2>
+                                <p class="mt-30 mb-30">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                </p>
+                                <button class="btn btn-blue">view products</button>
+                            </div>
+                        </div>
+                        <img src="assets/img/products/pro-3.png" alt="" class="img-responsive w-100">
+                    </div>
+                    <div class="bottom">
+                        <div class="col-xs-12 no-padding">
+                            <div class="col-sm-4 col-md-3 content-div">
+                                <div class="products-content text-right">
+                                    <h2>Refrigeration
+                                        <br> Service Parts
+                                    </h2>
+                                    <p class="mt-30 mb-30">
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                    </p>
+                                    <button class="btn btn-blue">view products</button>
+                                </div>
+                            </div>
+                            <img src="assets/img/products/pro-4.png" alt="" class="img-responsive w-100">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="item">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 no-padding top">
+                        <div class="col-sm-4 col-md-3 content-div">
+                            <div class="products-content text-right">
+                                <h2>Refrigeration
+                                    <br> Accessories &amp; Tools
+                                </h2>
+                                <p class="mt-30 mb-30">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                </p>
+                                <button class="btn btn-blue">view products</button>
+                            </div>
+                        </div>
+
+
+                        <img src="assets/img/products/pro-1.png" alt="" class="img-responsive w-100">
+                    </div>
+                    <!-- <div class="bottom">
+                        <div class="col-xs-12 no-padding">
+                            <div class="col-sm-4 col-md-3 content-div">
+                                <div class="products-content text-right">
+                                    <h2>Refrigeration
+                                        <br> Compressor
+                                    </h2>
+                                    <p class="mt-30 mb-30">
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                                    </p>
+                                    <button class="btn btn-blue">view products</button>
+                                </div>
+                            </div>
+                            <img src="assets/img/products/pro-2.png" alt="" class="img-responsive w-100">
+                        </div>
+                    </div> -->
+
+                </div>
+            </div>
+        </section>
+    </section>
+    <section class="product-list visible-xs">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 mt-20 mb-20">
+                    <img src="assets/img/products/ref-1.png" alt="" class="img-responsive">
+                    <div class="products-content text-center">
+                        <h2>Refrigeration
+                            <br> Compressor
+                        </h2>
+                        <p class="mt-30 mb-30">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                        </p>
+                        <button class="btn btn-blue">view products</button>
+                    </div>
+                </div>
+                <div class="col-xs-12 mt-20 mb-20">
+                    <img src="assets/img/products/ref-2.png" alt="" class="img-responsive">
+                    <div class="products-content text-center">
+                        <h2>Refrigeration
+                            <br> Replacement Condensing Unit &
+                            <br>Evaporators
+                        </h2>
+                        <p class="mt-30 mb-30">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                        </p>
+                        <button class="btn btn-blue">view products</button>
+                    </div>
+                </div>
+                <div class="col-xs-12 mt-20 mb-20">
+                    <img src="assets/img/products/ref-3.png" alt="" class="img-responsive">
+                    <div class="products-content text-center">
+                        <h2>Refrigeration
+                            <br> compressor parts
+                        </h2>
+                        <p class="mt-30 mb-30">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                        </p>
+                        <button class="btn btn-blue">view products</button>
+                    </div>
+                </div>
+                <div class="col-xs-12 mt-20 mb-20">
+                    <img src="assets/img/products/ref-4.png" alt="" class="img-responsive">
+                    <div class="products-content text-center">
+                        <h2>Refrigeration
+                            <br> Service Parts
+                        </h2>
+                        <p class="mt-30 mb-30">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                        </p>
+                        <button class="btn btn-blue">view products</button>
+                    </div>
+                </div>
+                <div class="col-xs-12 mt-20 mb-20">
+                    <img src="assets/img/products/ref-5.png" alt="" class="img-responsive">
+                    <div class="products-content text-center">
+                        <h2>Refrigeration
+                            <br> Accessories &amp; Tools
+                        </h2>
+                        <p class="mt-30 mb-30">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, facere.
+                        </p>
+                        <button class="btn btn-blue">view products</button>
+                    </div>
+                </div>
+            </div>
+    </section>
+</section>
+<?php include('includes/footer.php') ?>
