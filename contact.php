b9d9e2b7f1d1479dcc5b61ff608d8fc808254e29
amalkriz <amalkhd@gmail.com>
2018-04-10 10:01:10 +0530

    contact

---
+++ contact.php
@@ -0,0 +1,105 @@
+<?php include('includes/header.php') ?>
+<section class="contact">
+    <section class="contact-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-xs-12 left-content">
+                    <h1>TALK
+                        <br>
+                        <span>TO US</span>
+                    </h1>
+                    <p>Bringing You the Industry's Most Competitive Pricing for All of Your HVAC needs...</p>
+                </div>
+                <div class="col-sm-6 col-xs-12 right-content">
+                    <button class="btn btn-white">Quick Contact</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact-content bg-light-grey">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 text-center">
+                    <h3 class="cool-grey">Contact Us</h3>
+                    <p class="text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptate inventore laudantium temporibus
+                        quis. Commodi ipsum nostrum eveniet eos nulla.</p>
+                    <div class="row">
+                        <section class="m-tp-60">
+                            <div class="col-sm-4 text-center text-grey">
+                                <h5>Address 1</h5>
+                                <p>
+                                    Keas 69 Str.
+                                    <br> 15234, Chalandri
+                                    <br> Athens,
+                                    <br> Greece
+                                </p>
+                            </div>
+                            <div class="col-sm-4 text-center text-grey">
+                                <h5>Address 1</h5>
+                                <p>
+                                    Keas 69 Str.
+                                    <br> 15234, Chalandri
+                                    <br> Athens,
+                                    <br> Greece
+                                </p>
+                            </div>
+                            <div class="col-sm-4 text-center text-grey">
+                                <h5>Address 1</h5>
+                                <p>
+                                    Keas 69 Str.
+                                    <br> 15234, Chalandri
+                                    <br> Athens,
+                                    <br> Greece
+                                </p>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.835127609804!2d75.77210094982902!3d11.273527252865765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65ecdcf9be993%3A0x801e9f22dd52265c!2sWebqua+Software+Solutions!5e0!3m2!1sen!2sin!4v1523334502780"
+            width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </section>
+    <section class="enquiry m-tp-80">
+        <div class="container">
+            <div class="col-sm-8 col-sm-offset-2 text-center">
+                <div class="enquiry-text">
+                    <h3>Get In Touch</h3>
+                    <h5 class="mt-20">Our personal advisor will be in touch within 2 working days.</h5>
+                    <p class="mt-30 mb-30">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia consectetur accusamus a iure facere
+                        tempore voluptatem culpa quibusdam omnis sequi.</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-sm-offset-3">
+                <div class="row">
+                    <form class="enquiry-form">
+                        <div class="col-sm-6 mt-20">
+                            <input type="text" class="form-control" placeholder="First Name">
+                        </div>
+                        <div class="col-sm-6 mt-20">
+                            <input type="text" class="form-control" placeholder="Last Name">
+                        </div>
+                        <div class="col-sm-6 mt-20">
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="col-sm-6 mt-20">
+                            <input type="text" class="form-control" placeholder="Phone">
+                        </div>
+                        <div class="col-sm-12 mt-20">
+                            <textarea placeholder="Message" class="form-control" cols="30" rows="6"></textarea>
+                        </div>
+                        <div class="col-sm-12 mt-20 text-center">
+                            <button type="submit" class="btn btn-submit">SUBMIT</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</section>
+<?php include('includes/footer.php') ?>
