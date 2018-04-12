b1400cf1ed553d012f571097fcb6685e1a26c583
amalkriz <amalkhd@gmail.com>
2018-04-12 13:31:54 +0530

    categories

---
+++ categories.php
@@ -0,0 +1,110 @@
+<?php include('includes/header.php') ?>
+<section class="cat-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-xs-12 left-content">
+                <h1>REFRIGERATION
+                    <br>
+                    <span>COMPRESSOR</span>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="col-sm-6 col-xs-12 right-content">
+                <a href="contact.php#contact" class="btn btn-white">Quick Contact</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cat-list">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 mb-40">
+                <h2 class="text-grey">Refrigeration compressor parts</h2>
+            </div>
+            <div class="col-sm-6 mt-20 mb-40">
+                <div class="row">
+                    <div class="col-sm-8 col-xs-10">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 no-padding">
+                        <button class="btn btn-default">
+                            <i class="fa fa-filter"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-1.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+                    <hr>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-2.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+                    <hr>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-3.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-1.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-2.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cat-item">
+                    <img src="assets/img/inner/rel-3.png" alt="" class="img-responsive">
+                    <h4 class="text-grey">Piston</h4>
+                    <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consequatur inventore esse illo voluptatem
+                        ex libero ea velit tenetur? Itaque!</p>
+                    <a href="details.php">More details</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include('includes/footer.php') ?>
